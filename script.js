1ca4c70a916e26da5ef7ce2dfeabba6205d17c3d
CytechNaRak <nutdanai.namo2@gmail.com>
2026-01-08 19:12:45 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tab-content" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="nav-btn" and remove the class "active"
+    tablinks = document.getElementsByClassName("nav-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function copyText(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        showToast();
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+
+function showToast() {
+    var x = document.getElementById("toast");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
